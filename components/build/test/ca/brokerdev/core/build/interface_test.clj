ad9df35c2dff2099fed9da5d126b5fd6ab44dd8a
Eric Sigurdson <eric.sigurdson@brokerdev.ca>
2026-04-25 12:14:23 -0400

    fix(web): include http-client in test deps

---
+++ components/build/test/ca/brokerdev/core/build/interface_test.clj
@@ -0,0 +1,6 @@
+(ns ca.brokerdev.core.build.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [ca.brokerdev.core.build.interface :as build]))
+
+(deftest dummy-test
+  (is (= 1 1)))
