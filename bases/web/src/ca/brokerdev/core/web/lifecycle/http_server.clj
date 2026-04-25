74a88801d796635f61ad5edaa978bc6b888d4751
esigs <eric.sigurdson@brokerdev.ca>
2026-04-25 11:43:38 -0400

    feat(spa): add release build with /app route for web serving (#12)

--- bases/web/src/ca/brokerdev/core/web/lifecycle/http_server.clj
+++ bases/web/src/ca/brokerdev/core/web/lifecycle/http_server.clj
@@ -1,8 +1,9 @@
 (ns ca.brokerdev.core.web.lifecycle.http-server
-  (:require 
+  (:require
     [ca.brokerdev.core.lifecycle.interface :as lifecycle]
     [ca.brokerdev.core.http-server.interface :as http-server]
-    
+
+    [ca.brokerdev.core.web.pages.app :as app]
     [ca.brokerdev.core.web.pages.home :as home]
     ))
 
@@ -12,16 +13,22 @@
 (defmethod lifecycle/halt-key :web/http-server [_ system]
   (http-server/stop! system))
 
-(defn- home-hanlder [_req]
+(defn- home-handler [_req]
   {:status  200
    :body    (home/page)})
+
+(defn- app-handler [_req]
+  {:status  200
+   :headers {"content-type" "text/html"}
+   :body    (app/page)})
 
 (defn- not-found [_req]
   {:status  404
    :body    {:error "Custom not found"}})
 
-(def routes 
-  [["/"         {:name ::home  :get  {:handler home-hanlder}}]])
+(def routes
+  [["/"    {:name ::home :get {:handler home-handler}}]
+   ["/app" {:name ::app  :get {:handler app-handler}}]])
 
 (defmethod lifecycle/init-key :web/handler [_ system]
   (http-server/build-handler (assoc system
